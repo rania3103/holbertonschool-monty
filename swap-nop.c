ad09484332c9466b8672a814421ac1bda986086d
rania3103 <raniaabassi33@gmail.com>
2023-09-08 13:56:21 +0100

    push file

---
+++ swap-nop.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+/**
+ * swap -  swaps the top two elements of the stack.
+ * @stack: the top of the stack
+ * @line_number: line number
+*/
+void swap(stack_t **stack, line_number)
+{
+	int tmp;
+
+	if (!(stack) || !(*stack) || !(*stack->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short", line_number);
+		exit(EXIT_FAILURE)
+	}
+	tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
+/**
+ * nop - doesn’t do anything..
+ * @stack: the top of the stack
+ * @line_number: line number
+*/
+void nop(stack_t **stack, line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
