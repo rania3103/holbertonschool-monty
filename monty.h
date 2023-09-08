93c6e39f9c4c4a1f1dba6b49e0f3ffaf10011c54
amaalyy <amal.elzowawi@gmail.com>
2023-09-08 21:21:32 +0100

    fix errors

--- monty.h
+++ monty.h
@@ -35,13 +35,13 @@ typedef struct instruction_s
 } instruction_t;
 
 int main(int argc, char *argv[]);
-char **tokenize_line(char *line)
+char **tokenize_line(char *line);
 void execute_opcode(char **tokens);
 void push(stack_t **stack, int line_number);
-void pint(stack_t **stack, line_number);
-void pop(stack_t **stack, line_number);
-void swap(stack_t **stack, line_number);
-void add(stack_t **stack, line_number);
-void nop(stack_t **stack, line_number);
-void pall(stack_t **stack, line_number);
+void pint(stack_t **stack, int line_number);
+void pop(stack_t **stack, int line_number);
+void swap(stack_t **stack, int line_number);
+void add(stack_t **stack, int line_number);
+void nop(stack_t **stack, int line_number);
+void pall(stack_t **stack, int line_number);
 #endif
