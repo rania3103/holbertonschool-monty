75e87a4d769937020a755f050a446785c2a05b70
rania3103 <raniaabassi33@gmail.com>
2023-09-08 14:14:25 +0100

    fix if statement

--- execute.c
+++ execute.c
@@ -19,7 +19,7 @@ void execute_opcode(char *opcode, int value)
 
 	int i;
 
-	for(i = 0; instructions.opcode != NULL; i++)
+	for(i = 0; instructions[i].opcode != NULL; i++)
 	{
 		if (strcmp(instructions[i].opcode, opcode) == 0)
 		{
