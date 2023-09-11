da87e5e3d16498ef98c5dde08703502f243f1d7f
rania3103 <raniaabassi33@gmail.com>
2023-09-11 11:41:59 +0100

    fix errors

---
+++ push.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * push - pushes an  element to the stack;
+ * @stack: top of the stack
+ * @line_number:integer
+ * @value:integer
+*/
+void push(stack_t **stack, int line_number, int value)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	if (*stack)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
