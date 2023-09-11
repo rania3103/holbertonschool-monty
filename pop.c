ba91fb5eea3fee36f69e5e5b76a806f3fef59eab
Mariam Charrada <charradamariam@yahoo.com>
2023-09-11 14:28:48 -0700

    betty fix

--- pop.c
+++ pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 
 	/** removing the head node */
