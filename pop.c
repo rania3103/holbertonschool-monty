752ab396cc19f602128f558692e80807b59e414c
Mariam Charrada <charradamariam@yahoo.com>
2023-09-11 14:16:02 -0700

    pop

---
+++ pop.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+/**
+ * pop - Removes the topmost element from the stack
+ * @stack: Pointer to the stack head
+ * @line_number: The line number where the pop opcode was encountered
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head = *stack;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+exit(EXIT_FAILURE);
+	}
+
+	/** removing the head node */
+	*stack = head->next;
+
+	free(head);
+}
