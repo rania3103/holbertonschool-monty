8ed32761fd78e3f3835f12f96ced6abc136fffc0
rania3103 <raniaabassi33@gmail.com>
2023-09-11 14:27:32 +0100

    fix errors

--- monty.h
+++ monty.h
@@ -13,7 +13,7 @@
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIF
+ * for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
