74a52fc5d5d88f7f0d349b4fd5a2a39a86caea00
Mariam Charrada <charradamariam@yahoo.com>
2023-09-12 03:58:39 -0700

    execute v1

--- execute.c
+++ execute.c
@@ -1,84 +1,40 @@
-#include "monty.h"
+nclude "monty.h"
 /**
- * get_opcode - gets the operation
- * @stack: pointer to the head of the stack
- * @line_number: line number
- * @opcode: the operation
- * Return: 0 (on success) or -1
+ * execute_opcode - execute instruction
+ * @stack:top of the stack.
+ * @line_number: integer.
+ * @opcode:instruction.
+ * @value: integer.
 */
 
-int get_opcode(stack_t **stack, unsigned int line_number, char *opcode)
+void execute_opcode(stack_t **stack, unsigned int line_number, char *opcode, int value)
 {
-	instruction_t instruction[] = {
-	{"push", push},
-	{"pint", pint},
-	{"pop", pop},
-	{"swap", swap},
-	// {"add", add},
-	{"nop", nop},
-	{"pall", pall},
-	{NULL, NULL}
-	};
+        instruction_t instructions[] = {
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        // {"add", add},
+        {"nop", nop},
+        {"pall", pall},
+        {NULL, NULL}
+        };
 
-	int i;
+        int i;
 
-	for (i = 0; instruction[i].opcode != NULL; i++)
-	{
-		if (strcmp(opcode, instruction[i].opcode) == 0)
-		{
-			instruction[i].f(stack, line_number);
-			return (0);
-		}
-	}
-	return (-1);
+        for (i = 0; instructions[i].opcode != NULL; i++)
+        {
+                if (strcmp(opcode, "push") == 0)
+                {
+                        push(stack, line_number, value);
+
+                }
+                else if (strcmp(instructions[i].opcode, opcode) == 0)
+                {
+                        instructions[i].f(stack, line_number);
+                        return;
+                }
+        }
+        fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+        exit(EXIT_FAILURE);
 }
 
-
-/**
- * execute_opcode - executes monty instruction
- * @stack: the stack
- * @file: the file to be used
-*/
-void execute_opcode(FILE *file, stack_t **stack)
-{
-	int r, nline = 0;
-	size_t len = 0;
-	char *line = NULL, *token = NULL;
-
-	r = getline(&line, &len, file);
-	while (r > 0)
-	{
-		nline++;
-		token = strtok(line, " \n\t\r");
-
-		if (token == NULL)
-			continue;
-
-		if (get_opcode(stack, nline, token) == -1)
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", nline, token);
-			free(line);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	free(line);
-}
-/**	{
-			if (isdigit(tokens[1]))
-			{
-				value = atoi(tokens[1]);
-				execute_opcode(&stack, nline, tokens[0], value);
-			}
-			else
-			{
-				execute_opcode(&stack, nline, tokens[0], 0);
-			}	
-			nline++;
-			free(tokens);
-		}
-		else
-			continue;
-
-		 r = getline(&line, &len, file);	
-	}*/
