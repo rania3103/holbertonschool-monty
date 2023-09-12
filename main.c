56957ea0f1922b03ba1326d71ada22610ba28cfe
amaalyy <amal.elzowawi@gmail.com>
2023-09-12 12:52:25 +0100

    fix betty

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
@@ -29,23 +29,23 @@ int main(int argc, char *argv[])
 	}
 
 	r = getline(&line, &len, file);
-        while (r > 0)
-        {
-                tokens = tokenize_line(line);
-                if (isdigit(tokens[1]))
-                {
-                        value = atoi(tokens[1]);
-                        execute_opcode(&stack, nline, tokens[0], value);
-                }
-                else
-                {
-                        fprintf(stderr, "L%d: usage: push integer\n", nline);
-                        exit(EXIT_FAILURE);
-                }
-                nline++;
-                free(tokens);
-        }
-        free(line);
+	while (r > 0)
+	{
+		tokens = tokenize_line(line);
+		if (isdigit(tokens[1]))
+		{
+			value = atoi(tokens[1]);
+			execute_opcode(&stack, nline, tokens[0], value);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", nline);
+			exit(EXIT_FAILURE);
+		}
+		nline++;
+		free(tokens);
+	}
+	free(line);
 	fclose(file);
 	return (0);
 }
