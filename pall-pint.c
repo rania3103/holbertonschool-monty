a2bcbe843b335901cb02c6821176ff5edd38f589
amaalyy <amal.elzowawi@gmail.com>
2023-09-12 12:54:40 +0100

    fix betty

--- pall-pint.c
+++ pall-pint.c
@@ -9,7 +9,7 @@ void pall(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	stack_t *node = *stack;
 
-	while(node != NULL)
+	while (node != NULL)
 	{
 		printf("%d\n", node->n);
 		node = node->next;
