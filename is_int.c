fb8ccd1498f64b13cad3135e654bb272acbd01ce
rania3103 <raniaabassi33@gmail.com>
2023-09-15 11:15:05 +0100

    add is_int function

---
+++ is_int.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * is_int - check if the value is numeric.
+ * @value: string.
+ * Return:0 or 1
+*/
+int is_int(char *value)
+{
+	size_t i = 0;
+
+	if (value == NULL)
+	{
+		return (0);
+	}
+	while (i < strlen(value))
+	{
+		if (!isdigit(value[i]))
+		{
+			return(0);
+		}
+		i++;
+	}
+	return (1);
+}
