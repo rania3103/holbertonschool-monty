0a44a945a0d2aacdbf04826c38516ee885c2af6b
Mariam Charrada <charradamariam@yahoo.com>
2023-09-15 04:27:37 -0700

    betty fix

--- free.c
+++ free.c
@@ -1,11 +1,12 @@
 #include "monty.h"
 /**
-*free_stack - frees a stack
-@stack:the stack
+ * free_stack - frees a stack
+ * @stack: the stack
 */
 void free_stack(stack_t *stack)
 {
 	stack_t *tmp = stack;
+
 	while (tmp)
 	{
 		free(tmp);
